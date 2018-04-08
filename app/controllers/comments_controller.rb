22c1aed153904c800c23c8a56657a77f46f8bc2f
Kotaro Fukuo <fukuo@sansan.com>
2018-04-08 15:15:15 +0900

    :art: アクションの前にする処理を共通化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :find_from_params, only: [:create, :destory]
+
   def create
-    @post = Post.find(params[:post_id])
     @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
-  
+
   def destroy
-     @post = Post.find(params[:post_id])
      @post.comments.find(params[:id]).destroy
      redirect_to post_path(@post)
   end
-    
-    private 
+
+    private
     def comment_params
       params.require(:comment).permit(:body)
+    end
+
+    def find_from_params
+      @post = Post.find(params[:post_id])
     end
 end
