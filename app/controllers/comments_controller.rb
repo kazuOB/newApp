fbc280b99a8e65f93f8d3221bc4e0b5d1167dc20
Kotaro Fukuo <fukuo@sansan.com>
2018-04-08 15:15:39 +0900

    :art: インデントを揃える

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,16 +7,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-     @post.comments.find(params[:id]).destroy
-     redirect_to post_path(@post)
+    @post.comments.find(params[:id]).destroy
+    redirect_to post_path(@post)
   end
 
-    private
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
+  private
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 
-    def find_from_params
-      @post = Post.find(params[:post_id])
-    end
+  def find_from_params
+    @post = Post.find(params[:post_id])
+  end
 end
